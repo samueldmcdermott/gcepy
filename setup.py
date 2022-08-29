5c1d39ed6fd96163ffb707dae749687698b736dc
Samuel D. McDermott <samueldmcdermott@gmail.com>
2022-08-29 10:21:06 -0500

    setup file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="gcepy",
+    version="0.1",
+    author="Samuel D. McDermott",
+    author_email="samueldmcdermott@gmail.com",
+    description="likelihoods and samplers for the Galactic center excess",
+    packages=["gcepy"],
+    url="https://github.com/samueldmcdermott/gcepy",
+    install_requires=['setuptools',"jax", "dynesty", "numpyro"]
+)
