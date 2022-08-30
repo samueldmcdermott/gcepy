1129d783b6857ec7eceaaeb28b30a62e64659b29
Samuel D. McDermott <samueldmcdermott@gmail.com>
2022-08-30 13:01:04 -0500

    added __init__.py

---
+++ gcepy/__init__.py
@@ -0,0 +1,7 @@
+"""
+code for sampling for testing models of the Galactic center excess (GCE)
+must import highdim_model and lowdim_model separately
+data for lowdim_model is supplied and will run out of the box
+data for highdim_model must be requested and patched in manually or via appropriate edit to setup.py
+"""
+
