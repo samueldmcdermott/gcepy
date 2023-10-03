f0050e1b7d00984387f2b0948a9023559bf629ba
samueldmcdermott <samueldmcdermott@gmail.com>
2023-10-03 15:23:02 -0400

    updated for cleaner imports

--- scripts/Hrun.py
+++ scripts/Hrun.py
@@ -1,5 +1,5 @@
 import sys
-import gcepy.highdim_model as hm
+import gcepy as gp
 import dynesty as dn
 import numpyro as nr
 from jax import jit
@@ -8,7 +8,7 @@ import numpy as np
 import jax.random as jr
 
 def ptform(x, dim):
-    return x*(hm.pmax[:dim]-hm.pmin[:dim])+hm.pmin[:dim]
+    return x*(gp._hm.pmax[:dim]-gp._hm.pmin[:dim])+gp._hm.pmin[:dim]
 jptform = jit(ptform, static_argnums=(1,))
 
 EBIN, EXNUM = int(sys.argv[1]), int(sys.argv[2])
@@ -16,11 +16,11 @@ EBIN, EXNUM = int(sys.argv[1]), int(sys.argv[2])
 modparams = [18, 19, 19, 19, 19, 20, 20]
 PDIM = modparams[EXNUM]
 
-sampler_dn = dn.NestedSampler(lambda x: hm.jjlnlike(x, EBIN, ex_num=EXNUM), lambda x: jptform(x, PDIM), PDIM)
+sampler_dn = dn.NestedSampler(lambda x: gp.lnlike('high', x, EBIN, ex_num=EXNUM), lambda x: jptform(x, PDIM), PDIM)
 
 sampler_dn.run_nested(dlogz=1)
 
-sampler_nr = nr.infer.MCMC(nr.infer.NUTS(potential_fn = lambda t: -hm.jjlnprob(t, bin_no=EBIN, ex_num=EXNUM)), num_samples=10000, num_warmup=100, jit_model_args=True, chain_method='vectorized')
+sampler_nr = nr.infer.MCMC(nr.infer.NUTS(potential_fn = lambda t: -gp.lnlike('high', t, bin_no=EBIN, ex_num=EXNUM)), num_samples=10000, num_warmup=100, jit_model_args=True, chain_method='vectorized')
 
 rng_key = jr.PRNGKey(0)
 
@@ -28,7 +28,7 @@ sampler_nr.run(rng_key, init_params = sampler_dn.results.samples[-1])
 
 samples_nr = sampler_nr.get_samples()
 
-lls_nr = jnp.array([hm.jjlnprob(x, bin_no=EBIN, ex_num=EXNUM) for x in samples_nr])
+lls_nr = jnp.array([gp.lnlike('high', x, bin_no=EBIN, ex_num=EXNUM) for x in samples_nr])
 
 sampler_dn.results.logl[-1], float(lls_nr.max()), float(-sampler_nr.last_state.potential_energy), int(lls_nr.argmax())
 
