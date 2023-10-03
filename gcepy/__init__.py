6e620aa3a8e97943c6e89ebec9bb28985734488c
samueldmcdermott <samueldmcdermott@gmail.com>
2023-10-03 14:51:41 -0400

    removed some vestigial `poetry` stuff, moved run scripts to `scripts`

--- gcepy/__init__.py
+++ gcepy/__init__.py
@@ -1,9 +1,11 @@
 """
 code for sampling for testing models of the Galactic center excess (GCE)
-can use highdim_model and lowdim_model separately, or just interfact with lnlike()
+can use highdim_model and lowdim_model separately, or use lnlike() directly
 data for lowdim_model is supplied and will run out of the box
 data for highdim_model must be requested and patched in manually or via appropriate edit
 """
+
+__version__ = "0.2"
 
 from gcepy.lowdim_model import jjlnprob as _lmlnprob
 from gcepy.highdim_model import jjlnprob as _hmlnprob
