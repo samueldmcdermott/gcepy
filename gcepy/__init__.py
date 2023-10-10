75ca842b8437178492f757df07664c2457233e49
samueldmcdermott <samueldmcdermott@gmail.com>
2023-10-10 09:41:45 -0400

    now works if hm not available

--- gcepy/__init__.py
+++ gcepy/__init__.py
@@ -9,14 +9,23 @@ __version__ = "0.2"
 
 import gcepy.lowdim_model as _lm
 del(lowdim_model)
-import gcepy.highdim_model as _hm
-del(highdim_model)
+
+try:
+    import gcepy.highdim_model as _hm
+    del(highdim_model)
+    no_hm = False
+except FileNotFoundError:
+    no_hm = True
+    print("high dimensional model not available")
 
 def lnlike(model, *args, **kwargs):
     if 'l' in model:
         return _lm.jjlnprob(*args, **kwargs)
     elif 'h' in model:
-        return _hm.jjlnprob(*args, **kwargs)
+        if no_hm:
+            print("high dimensional model not available")
+            pass
+        else:
+            return _hm.jjlnprob(*args, **kwargs)
     else:
-        print("must include 'l' (for low-dimensional) or 'h' (for high-dimensional) when you specify the first "
-              "argument, please")
+        print("first argument must include 'l' (for low-dimensional) or 'h' (for high-dimensional), please")
